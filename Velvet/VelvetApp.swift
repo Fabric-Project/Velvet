7b86dcfa44078d3b0bbe04579c8ae733f6276290
vade <vade@vade.info>
2025-04-16 19:31:16 -0400

    Initial Commit

---
+++ Velvet/VelvetApp.swift
@@ -0,0 +1,17 @@
+//
+//  VelvetApp.swift
+//  Velvet
+//
+//  Created by Anton Marini on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct VelvetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
